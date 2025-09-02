11ccabcc8529d06aa10468ea8ed9d82128cbddd5
Muhammad Asifur Rahman <asifrahman@arizona.edu>
2025-09-02 01:18:15 -0700

    working on data processing

---
+++ src/fetch_api.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+def fetch_incident_data():
+    # load the JSON data as if fetched from an API
+    file_path = os.path.join(os.path.dirname(__file__), "../data/sampleIncidentData.json")
+    with open(file_path, "r") as file:
+        data = json.load(file)
+
+    return data
+
+# testing program
+if __name__ == "__main__":
+    incidents = fetch_incident_data()
+    print(f"Fetched {len(incidents)} incident reports: ")
+
+    for incident in incidents:
+        print(f" - {incident["incident_id"]}: {incident["status"]}")
